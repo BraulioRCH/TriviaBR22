6951bd413750197464e4b35f84d2a306ec31ecd8
BraulioRCH <juan.ramirez@ucrso.info>
2022-12-03 12:05:32 -0600

    Initial commit :tada:

---
+++ Assets/Scrips/PanelComplementario.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PanelComplementario : MonoBehaviour
+{
+    public GameObject panelComplementario;
+
+    public Game game;
+
+    public void continuar()
+    {
+        panelComplementario.SetActive(true);
+        game.setPregunta();
+    }
+
+    public void desplegar()
+    {
+        panelComplementario.SetActive(true);
+    }
+
+
+
+}
